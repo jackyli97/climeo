9aec163479b21d0fafccf83b1b2783be842ec913
jackyli97 <xwlh808@gmail.com>
2021-01-04 22:52:36 -0800

    Complete wikit

---
+++ chrome_extensions/wikit/eventPage.js
@@ -0,0 +1,26 @@
+let menuItem = {
+    "id": "wikit",
+    "title": "Wikit",
+    "contexts": ["selection"]
+};
+chrome.contextMenus.create(menuItem);
+
+function fixedEncodeURI (str) {
+    return encodeURI(str).replace(/%5B/g, "[").replace(/%5B/g, "]");
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == "wikit" && clickData.selectionText){
+        let wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
+
+        var createData = {
+            "url": wikiUrl,
+            "type": "popup",
+            "top": 5,
+            "left": 5,
+            "width": screen.availWidth/2,
+            "height": screen.availHeight/2
+        };
+        chrome.windows.create(createData, function(){});
+    };
+});
